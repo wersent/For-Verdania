13b142db8b0ca94808f79f160977cce1c763fb6f
wersent <gasgleb@gmail.com>
2025-04-01 20:27:44 +0200

    Adding battle mechanic (v 0.2.7)

--- Assets/Scripts/ScrollViewHandler.cs
+++ Assets/Scripts/ScrollViewHandler.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScrollViewHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ButtonClicked()
+    {
+        gameObject.SetActive(true);
     }
 }

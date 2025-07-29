b898698688fa7e54665feb71f07e39d2ca29b850
wersent <gasgleb@gmail.com>
2025-07-29 11:41:38 +0200

    fix mistake

--- Assets/Scripts/TestPanel2.cs
+++ Assets/Scripts/TestPanel2.cs
@@ -1,29 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 /*
-public class TestUI : MonoBehaviour
+public class TestPanel2 : MonoBehaviour, ISetParentAndChildrenDisable
 {
-    public UnitRegiment unit;
-    public TextMeshProUGUI tmp;
+    [SerializeField] protected ScrollViewHandler _scrollViewHandler;
+    [SerializeField] private UnitRegiment _unitRegiment;
+    [SerializeField] private Image _menuBackground;
 
     // Start is called before the first frame update
     void Start()
     {
-        tmp = GetComponent<TextMeshProUGUI>();
-        //unit.OnHpChangedEvent += UpdateHp;
-    }
-
-    public void UpdateHp(int hp)
-    {
-        tmp.text = $"Hp: {hp}";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Initialize(ScrollViewHandler scrollViewHandler)
+    {
+        _scrollViewHandler = scrollViewHandler;
+
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.TryGetComponent<UnitCompositionController>(out var unitCompositionController)) unitCompositionController.Initialize(_scrollViewHandler);
+        }
+    }
+
+    public void SetParentAndChildrenDisable()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.TryGetComponent<IButtonClicked>(out var buttonClicked)) child.gameObject.SetActive(false);
+        }
+        gameObject.SetActive(false);
     }
 }
 */
